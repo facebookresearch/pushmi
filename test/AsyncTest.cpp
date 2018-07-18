920b989824f0c211988a276704a4002aaf671921
Lee Howes <lwh@fb.com>
2018-07-18 11:00:09 -0700

    Added async_join

--- test/AsyncTest.cpp
+++ test/AsyncTest.cpp
@@ -82,6 +82,7 @@ namespace detail {
                         auto token = AsyncToken<
                             std::decay_t<decltype(v)>, std::decay_t<decltype(exec)>>{
                           exec, std::forward<decltype(v)>(v)};
+                        token.dataPtr_->cv_.notify_all();
                         ::pushmi::set_value(out, std::move(token));
                       }
                     )
@@ -125,6 +126,109 @@ PUSHMI_INLINE_VAR constexpr detail::async_fork_fn async_fork{};
 } // namespace pushmi
 
 
+namespace pushmi {
+namespace detail {
+  template<class Out>
+  struct async_join_fn_data : public Out {
+    async_join_fn_data(Out out) :
+      Out(std::move(out)) {}
+  };
+
+  template<class Out>
+  auto make_async_join_fn_data(Out out) -> async_join_fn_data<Out> {
+    return {std::move(out)};
+  }
+
+  struct async_join_fn {
+    auto operator()() const {
+      return constrain(lazy::Sender<_1>, [](auto in) {
+        using In = decltype(in);
+        return ::pushmi::detail::deferred_from<In, single<>>(
+          std::move(in),
+          ::pushmi::detail::submit_transform_out<In>(
+            constrain(lazy::Receiver<_1>, [](auto out) {
+              using Out = decltype(out);
+              return ::pushmi::detail::out_from_fn<In>()(
+                make_async_join_fn_data(std::move(out)),
+                // copy 'f' to allow multiple calls to submit
+                ::pushmi::on_value([](auto& data, auto&& asyncToken) {
+                  // Async version that does not - why not?
+                  using V = decltype(asyncToken);
+                  auto exec = asyncToken.e_;
+                  ::pushmi::submit(
+                    exec,
+                    ::pushmi::now(exec),
+                    ::pushmi::make_single(
+                      [asyncToken, out = std::move(static_cast<Out&>(data)), exec](auto) mutable {
+                        // Token hard coded for this executor type at the moment
+                        std::thread t([
+                           exec,
+                           asyncToken,
+                           out]() mutable {
+                          std::unique_lock<std::mutex> lk(asyncToken.dataPtr_->cvm_);
+                          // TODO: Currently this never wakes up. Fix to work properly.
+                          //asyncToken.dataPtr_->cv_.wait(lk);
+                          ::pushmi::submit(
+                            exec,
+                            ::pushmi::now(exec),
+                            ::pushmi::make_single(
+                              [asyncToken, out, exec](auto) mutable {
+                                // Token hard coded for this executor type at the moment
+                                ::pushmi::set_value(out, std::move(asyncToken.dataPtr_->v_));
+                              }
+                            ));
+                        });
+                        t.detach();
+                      }
+                    )
+                  );
+                }),
+                ::pushmi::on_error([](auto& data, auto e) noexcept {
+                  auto out = std::move(static_cast<Out&>(data));
+                  ::pushmi::set_error(out, std::move(e));
+                }),
+                ::pushmi::on_done([](auto& data){
+                  auto out = std::move(static_cast<Out&>(data));
+                  ::pushmi::set_done(out);
+                })
+              );
+            })
+          )
+        );
+      });
+    }
+  };
+
+} // namespace detail
+
+namespace operators {
+PUSHMI_INLINE_VAR constexpr detail::async_join_fn async_join{};
+} // namespace operators
+} // namespace pushmi
+
+// This should specialise using customisation points
+// For now it just assumes that this executor deals with threads and creates a
+// new thread for each node.
+/*
+auto async_join() {
+   return pushmi::make_single_deferred(
+     [](auto out){
+       return v::on_value([out = std::move(out)](auto asyncToken){
+         std::thread t([
+            asyncToken = std::move(asyncToken),
+            out = std::move(out)](){
+           std::unique_lock<std::mutex> lk(asyncToken.data_->cvm_);
+           asyncToken.data_->cv_.wait(lk);
+           op::via([](){return asyncToken.e_;}) |
+              v::on_value([asyncToken, out = std::move(out)](auto&){
+                ::pushmi::set_value(out, std::move(asyncToken.data_->v_));
+              });
+         });
+         t.detach();
+       });
+     });
+}*/
+
 SCENARIO( "async", "[async]" ) {
 
   GIVEN( "A new_thread time_single_deferred" ) {
@@ -142,6 +246,7 @@ SCENARIO( "async", "[async]" ) {
                 std::decay_t<decltype(v)>, std::decay_t<decltype(exec)>>{
               exec, std::forward<decltype(v)>(v)};
           }) |
+          op::transform([](auto v){auto ptr = v.dataPtr_; return v;}) |
           op::transform([](auto v){return v.dataPtr_->v_;}) |
           op::blocking_submit(v::on_value([&](auto v) { values.push_back(std::to_string(v)); }));
 
@@ -154,7 +259,8 @@ SCENARIO( "async", "[async]" ) {
         std::vector<std::string> values;
         auto realthing = op::just(2.0) |
           op::async_fork([&](){return nt;}) |
-          op::transform([](auto v){return v.dataPtr_->v_;}) |
+          op::transform([](auto v){auto ptr = v.dataPtr_; return v;}) |
+          op::async_join() |
           op::blocking_submit(v::on_value([&](auto v) { values.push_back(std::to_string(v)); }));
 
         THEN( "only the first item was pushed" ) {
