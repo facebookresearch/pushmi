9db2ff7815a31519e683f010c0d996a4b8cfd182
Kirk Shoop <kirk.shoop@gmail.com>
2018-10-16 14:06:22 -0700

    add missing copyright headers and license info

--- benchmarks/PushmiBenchmarks.cpp
+++ benchmarks/PushmiBenchmarks.cpp
@@ -1,6 +1,11 @@
 
 #include <vector>
 
+// Copyright (c) 2018-present, Facebook, Inc.
+//
+// This source code is licensed under the MIT license found in the
+// LICENSE file in the root directory of this source tree.
+
 #include "pushmi/o/just.h"
 #include "pushmi/o/defer.h"
 #include "pushmi/o/on.h"
--- buildSingleHeader.cmake
+++ buildSingleHeader.cmake
@@ -1,4 +1,9 @@
 
+# Copyright (c) 2018-present, Facebook, Inc.
+#
+# This source code is licensed under the MIT license found in the
+# LICENSE file in the root directory of this source tree.
+
 function(BuildSingleHeader HeaderName)
     set(header_files ${ARGN})
     # write out single-header
